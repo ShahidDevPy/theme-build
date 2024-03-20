163ec6d868cb0009d0d3113adb19f8461c7fbb1c
Shahid <shahidrasool.cs@gmail.com>
2024-03-20 07:39:55 +0500

    Fixed state reactivity for change theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,19 +3,20 @@ import ThemeSwitcher from "./ThemeSwitcher.tsx";
 const Header = () => {
 
     return (
-        <header className='flex items-center justify-between py-4 shadow shadow-gray-200 bg-baseOne transition-colors duration-300 lg:px-[160px] sm:px-[40px] px-[16px]'>
+        <header
+            className='flex items-center justify-between py-4 shadow shadow-gray-200 bg-baseOne transition-colors duration-300 lg:px-[160px] sm:px-[40px] px-[16px]'>
             <div>
-                <img className="w-[40px] h-[40px]" src={heroIcon} alt="icon" />
+                <img className="w-[40px]" src={heroIcon} alt="icon"/>
             </div>
             <nav className="sm:block hidden">
-                <ul className='flex items-center space-x-5'>
+                <ul className='flex text-baseTwo items-center space-x-5'>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Contact Us</a></li>
                 </ul>
             </nav>
             <div>
-                <button><strong>Select Theme</strong></button>
+                <button className='text-baseTwo'><strong>Select Theme</strong></button>
             </div>
             <ThemeSwitcher/>
         </header>
--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -3,6 +3,7 @@ import light from '../assets/dark.png';
 import dark from '../assets/dark.png';
 import third from '../assets/dark.png';
 
+
 type Theme = {
     src: string;
     alt: string;
@@ -10,17 +11,13 @@ type Theme = {
 };
 
 const themes: Theme[] = [
-    {src: light, alt: 'Light', name: 'light'},
-    {src: dark, alt: 'Dark', name: 'dark'},
-    {src: third, alt: 'Third', name: 'third'},
+    { src: light, alt: 'Light', name: 'light' },
+    { src: dark, alt: 'Dark', name: 'dark' },
+    { src: third, alt: 'Third', name: 'third' },
 ];
 
 const ThemeSwitcher = () => {
-    const [selectedTheme, setSelectedTheme] = useState('');
-
-    const handleThemeChange = (themeName: string) => {
-        setSelectedTheme(themeName);
-    };
+    const [selectedTheme, setSelectedTheme] = useState(() => localStorage.getItem('selectedTheme') || '');
 
     useEffect(() => {
         const prefersDark = window.matchMedia('(prefers-color-scheme: dark)');
@@ -29,7 +26,6 @@ const ThemeSwitcher = () => {
         const updateTheme = () => {
             const storedTheme = localStorage.getItem('selectedTheme');
             const setTheme = (theme: string) => {
-                console.log(theme)
                 document.documentElement.setAttribute('data-theme', theme);
                 setSelectedTheme(theme);
             };
@@ -61,13 +57,17 @@ const ThemeSwitcher = () => {
         };
     }, []);
 
+    const handleThemeChange = (themeName: string) => {
+        setSelectedTheme(themeName);
+        document.documentElement.setAttribute('data-theme', themeName);
+        localStorage.setItem('selectedTheme', themeName);
+    };
+
     return (
         <section className='bg-baseThree px-5 py-4 absolute lg:right-36 sm:right-12 right-4 top-24'>
             <div className='grid sm:grid-cols-3 grid-cols-1 gap-10'>
                 {themes.map((theme, index) => (
-                    <div
-                        className={`max-w-[150px] p-1 ${selectedTheme === theme.name && 'border-2 border-green-500 rounded-md'}`}
-                        key={index}>
+                    <div className={`max-w-[150px] p-1 ${selectedTheme === theme.name && 'border-2 border-green-500 rounded-md'}`} key={index}>
                         <label>
                             <input
                                 type='radio'
@@ -77,12 +77,10 @@ const ThemeSwitcher = () => {
                                 onChange={() => handleThemeChange(theme.name)}
                                 className='hidden'
                             />
-                            <img className='rounded-md cursor-pointer' src={theme.src} alt={theme.alt}/>
+                            <img className='rounded-md cursor-pointer' src={theme.src} alt={theme.alt} />
                             <div className='flex items-center justify-between mt-2'>
                                 <h5 className='capitalize text-sm text-baseTwo'>{theme.name} Mode</h5>
-                                <div
-                                    className='bg-green-500 rounded-full w-[20px] flex items-center justify-center text-white text-sm'>{selectedTheme === theme.name &&
-                                    <span>&#10003;</span>}</div>
+                                <div className='bg-green-500 rounded-full w-[20px] flex items-center justify-center text-white text-sm'>{selectedTheme === theme.name && <span>&#10003;</span>}</div>
                             </div>
                         </label>
                     </div>
